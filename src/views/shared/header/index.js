25896e2d1b57925f36b91ec652111a526be3e13f
ken <ken@fancplace.io>
2023-06-13 13:46:53 +0900

    feat: 공통 컴포넌트 적용

---
+++ src/views/shared/header/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export default function Header() {
+  return (
+    <Container>
+      Header
+    </Container>
+  )
+};
+
+const Container = styled.div`
+
+`;
