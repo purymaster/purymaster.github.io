22fb107792af8b09d44fceaaac771129289cb5bd
ken <ken@fancplace.io>
2023-06-19 14:42:46 +0900

    feat: 포트폴리오 상세 컴포넌트 적용

--- src/views/pages/works/components/Work.js
+++ src/views/pages/works/components/Work.js
@@ -1,23 +1,81 @@
 import styled from 'styled-components';
 import { useLocation } from 'react-router-dom';
+import { GitHub } from 'react-feather';
 
 export default function Work() {
-
   const location = useLocation();
   const data = location.state;
-  console.log(data);
 
   return (
     <Container>
       {data && (
         <>
-          <h1>{data.name}</h1>
+          <div className="img">
+            <img src={data.image.url} alt="" />
+          </div>
+          <div className="info">
+            <h1>{data.name}</h1>
+            <dl>
+              {data.taskDuration && (
+                <div>
+                  <dt>작업기간</dt>
+                  <dd>
+                    {data.taskDuration.startAt}~{data.taskDuration.endAt}
+                  </dd>
+                </div>
+              )}
+              {data.spec && (
+                <div>
+                  <dt>사용기술</dt>
+                  <dd>
+                    <ul>
+                      {data.spec.map((data) => (
+                        <li>{data}</li>
+                      ))}
+                    </ul>
+                  </dd>
+                </div>
+              )}
+              {data.info && (
+                <div>
+                  <dt>설명</dt>
+                  <dd>
+                    <ul>
+                      {data.info.map((data) => (
+                        <li>{data}</li>
+                      ))}
+                    </ul>
+                  </dd>
+                </div>
+              )}
+              {data.note && (
+                <div>
+                  <dt>비고</dt>
+                  <dd>
+                    <ul>
+                      {data.note.map((data) => (
+                        <li>{data}</li>
+                      ))}
+                    </ul>
+                  </dd>
+                </div>
+              )}
+              {data.link && (
+                <div>
+                  <dt>링크</dt>
+                  <dd>
+                    <a href={data.link.url} target="_blank" rel="noreferrer">
+                      <GitHub size={16} />
+                    </a>
+                  </dd>
+                </div>
+              )}
+            </dl>
+          </div>
         </>
       )}
     </Container>
   );
 }
 
-const Container = styled.div`
-
-`;
+const Container = styled.div``;
